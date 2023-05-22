0b410be07a52be46f129cfbcbe9171ac3fa6c7d9
muhamadjawad <jawadmansoor1998@gmail.com>
2023-05-22 16:55:36 +0500

    first commti

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {View} from 'react-native'
+
+const App = () => {
+  return <View style={{flex: 1, backgroundColor: 'red'}}></View>;
+};
+
+export default App;
